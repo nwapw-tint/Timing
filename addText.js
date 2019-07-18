bc4b1ba89d344b774c4f091aa9df7d268e132b52
Eric Xia <ericpingxia@gmail.com>
2019-07-18 09:21:52 -0700

    fixed text adding bug
    When the user attempts to display the time through ctrl+space, but there is no filter active.

--- addText.js
+++ addText.js
@@ -4,7 +4,11 @@ addText(text);
 function addText(text)
 {
     textDiv = document.getElementById("textDiv")
+    if(textDiv != null)
+    {
     textDiv.innerHTML = text;
     textDiv.style.opacity = 1;
     setTimeout(function(){textDiv.innerHTML = "";},2000);
+    }
+    else {console.log("the user tried to display the time but there is no filter active currently");}
 }
