661ae1e8daaf28d0ad9fa5eef5978fa959f3ed41
Eric Xia <ericpingxia@gmail.com>
2019-07-23 13:35:59 -0700

    fadein rewritten in RGBA

--- content.js
+++ content.js
@@ -133,11 +133,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 /*-----------------------Add Text-----------------------*/
 
-
-
-const fadeDuration = 1400;
-const fadeStep = 200;
-
 /*var robotoFont = document.createElement('link');
 robotoFont.setAttribute('rel', 'stylesheet');
 robotoFont.setAttribute('type', 'text/css');
@@ -159,33 +154,37 @@ function addText(text, time)
         }, 1000); //faux dynamic feeling
     }
 }
+fadeStep = 100;
+fadeDuration = 1400;
     //Fades the target element.
-    function fadeOut(fadeTarget, fadeStep, fadeDuration) {
+    function fadeOut(fadeTarget, fadeStep, fadeDuration) { //TODO: rewrite this
         var fadeEffect = setInterval(() => {
             if (!fadeTarget.style.opacity)
                 fadeTarget.style.opacity = 1;
             if (fadeTarget.style.opacity > 0.001)
                 fadeTarget.style.opacity -= fadeStep / fadeDuration;
             else {
-				alert('fade element is working')
                 clearInterval(fadeEffect);
                 fadeTarget.style.opacity = 0;
             }
         }, fadeStep);
 	}
+	//RGBA alpha value change
 	function fadeIn(fadeTarget, fadeStep, fadeDuration){
-		var fadeEffect = setInterval(() => {
-			if (!fadeTarget.style.opacity)
-				fadeTarget.style.opacity = 0;
-			if (fadeTarget.style.opacity <0.999)
-				fadeTarget.style.opacity += fadeStep / fadeDuration;
-			else {
-				clearInterval(fadeEffect);
-				fadeTarget.style.opacity = 1;
-			}
-		})
+		colorArray = fadeTarget.style.background.replace(/[^\d,.]/g, '').split(',');
+		newA = Number(colorArray[3]);
+		var fadeEffect = setInterval(() => 
+		{
+			if(newA < 0.99){
+				rgbaStr = "rgba(" + colorArray[0]+ "," + colorArray[1] + "," + colorArray[2] + "," + String(newA) + ")";
+				fadeTarget.style.background = rgbaStr;   
+				newA += fadeStep / fadeDuration;
+			}    
+			else{			
+				fadeTarget.style.background = "rgba(" + colorArray[0]+ "," + colorArray[1] + "," + colorArray[2] + "," + 1 + ")";
+				clearInterval(fadeEffect)}
+			}, fadeStep);
 	}
-var cycleInterval = 200;
 var rainbow = ['rgb(254,55,72)',
 'rgb(205, 26, 198)',
 'rgb(33, 134, 252)',
@@ -196,16 +195,22 @@ var rainbow = ['rgb(254,55,72)',
 'rgb(252, 118, 33)',
 'rgb(254,55,72)'];//SHOULD BE EVEN, mess with colors later
 cCycle = 0;
-spacer = 0;
+cSpacer = 0;
+cDuration = 0;
+ffadeStep = 100;
+ffadeDuration = 1400;
 function runCycleThenRemove(element)
 {
+	fadeIn(element, ffadeStep, ffadeDuration);
+	setTimeout(function(){
 		timer = setInterval(function() {
 			element.style.background = rainbow[cCycle];
-			spacer = Math.abs(cCycle-Math.floor(rainbow.length/2)) //mess with this stuff later
+			cSpacer = Math.abs(cCycle-Math.floor(rainbow.length/2)) //mess with this stuff later
 			cCycle++;
-		}, 200+spacer*80)
+		}, 200+cSpacer*80)
 		setTimeout(function(){clearInterval(timer);
-			fadeOut(element,fadeStep, fadeDuration)
-			element.parentNode.removeChild(element);
-		}, rainbow.length*cycleInterval*2);
+			fadeOut(element,ffadeStep, ffadeDuration)
+			//element.parentNode.removeChild(element);
+		}, 3600); //TODO: remove this
+	},ffadeDuration)
 }
