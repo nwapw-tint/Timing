76ed845f96c9ddbc2f51f7ad03e3ec56b7ca7001
rkique <ericpingxia@gmail.com>
2019-07-29 08:57:37 -0700

    Revert "Revert "Revert "blacklist alerting works"""
    This reverts commit 948d539678cd7ff5ef017303af65062668596db6.

--- background.js
+++ background.js
@@ -3,9 +3,11 @@ var sessionRunning = false, onChromeSite = false;
 
 var currentSite = "";
 var sitesVisited = [];
+var blacklistedSites = [];
 var timeout;
 
 var theme;
+
 //The hidden timer
 function timeoutUpdate() {
 	if (sessionRunning) {
@@ -124,6 +126,9 @@ chrome.extension.onConnect.addListener((port) => {
 		case "error":
 			alert(msg.error);
 			break;
+		case "blacklist":
+			updateSites(msg.sites);
+			break;
 		}
 	});
 	port.index = ports.length;
@@ -151,6 +156,21 @@ function sendMessage(msg) {
 
 
 /*-------------------------Update Content-------------------------*/
+
+//updates the array of blacklisted sites
+function updateSites(sites)
+{
+	blacklistedSites = sites.split("\n");
+	console.log(blacklistedSites);
+}
+var onBlacklistedSite = false;
+function isCurrentTabBlacklisted() {
+	let blacklisted = false;
+	for (let i = 0; i < blacklistedSites.length && !blacklisted; i++)
+		if (currentSite.url == blacklistedSites[i])
+			blacklisted = true;
+	onBlacklistedSite = blacklisted;
+}
 
 //Updates the content tint to the specified color
 function updateContentTint() {
@@ -191,16 +211,6 @@ function removeContentTint(){
 		from: "background",
 		action: "tint",
 		mode: "remove"
-	});
-}
-
-function sendBlackout(){
-	sendMessage({
-		to: "content",
-		from: "background",
-		action: "tint",
-		mode: "blackout",
-		color: getTint()
 	});
 }
 
@@ -268,6 +278,8 @@ function updatePopup() {
 
 /*-------------------------Chrome Functions-------------------------*/
 
+
+
 var runningBeforeOnChromeSite = false;
 
 //Checks the current site to see if it has been filtered. If it hasn't been visited, add it to visited.
@@ -282,7 +294,7 @@ chrome.tabs.onActivated.addListener((activeInfo) => {
 				url: tabs[0].url,
 				tabId: activeInfo.tabId
 			};
-			console.log("onActivated calls "+useBlacklist(currentSite.url));
+			
 			if (currentSite.url.indexOf("chrome://") == 0) {
 				onChromeSite = true;
 				updatePopupStartStopButton();
@@ -299,6 +311,9 @@ chrome.tabs.onActivated.addListener((activeInfo) => {
 					sessionRunning = true;
 					updatePopupSessionRunning();
 				}
+			}
+			if (onBlacklistedSite) {
+				alert("blacklisted!");
 			}
 			if (hasVisitedSite(currentSite)) {
 				updateContentTint();
@@ -321,41 +336,6 @@ chrome.tabs.onActivated.addListener((activeInfo) => {
 	});
 });
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	useBlacklist(tab.url);
-});
-
-chrome.tabs.onCreated.addListener(function(tab) {   
-	useBlacklist(tab.url);
-});
-
-foundUrl = false;
-function useBlacklist(url)
-{
-	var blacklist;
-	chrome.storage.sync.get('sites', function(items) {
-		if (typeof items.sites === 'undefined') 
-		{
-			console.log("we tried to check the blacklist, but it hasn't been set yet")
-		} 
-		else 
-		{
-			blacklist = items.sites.split('\n')
-			for(i = 0; i<blacklist.length; i++)
-			{
-				console.log("checking "+blacklist[i]+" against "+url+" which is "+url.includes(blacklist[i]))
-				if(url.includes(blacklist[i]))
-				{
-					foundUrl = true;
-				}
-			}
-		}
-	  });
-	if(foundUrl)
-	{
-		sendBlackout();
-	}
-}
 //Invoked with Ctrl+Space
 chrome.commands.onCommand.addListener((command) => {
 	if (command == "display_text" && sessionRunning) {
