41defb45c51a39c8ca7ba2e01d91d2068a07c4cc
Bruce Cosgrove <1901.brc@gmail.com>
2019-07-30 10:43:29 -0700

    Finally added time <= 0 check

--- scripts/popup/popup.js
+++ scripts/popup/popup.js
@@ -160,8 +160,8 @@ port.onMessage.addListener((msg) => {
 		break;
 	case "update":
 		switch (msg.place) {
-			case "ETA":
-			document.getElementById('session_label').innerHTML = "Sessions "+msg.text;
+		case "ETA":
+			document.getElementById('session_label').innerHTML = "Sessions " + msg.text;
 			break;
 		case "sessions":
 			sessions = msg.sessions;
@@ -213,6 +213,8 @@ document.addEventListener('DOMContentLoaded', () => {
 			showError("Time is not a number!");
 		} else if (time > maxTime) {
 			showError("Time is too long!");
+		} else if (time <= 0) {
+			showError("Time is too short");
 		} else {
 			addSession(time);
 		}
