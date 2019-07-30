0896579c73cb5aac12add748d51977eb6b53f563
Alexime <47611962+Alexime@users.noreply.github.com>
2019-07-30 10:56:10 -0700

    formatting

--- scripts/popup/popup.js
+++ scripts/popup/popup.js
@@ -161,7 +161,7 @@ port.onMessage.addListener((msg) => {
 	case "update":
 		switch (msg.place) {
 			case "ETA":
-			document.getElementById('session_label').innerHTML = "Sessions "+msg.text;
+			document.getElementById('session_label').innerHTML = "Sessions &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"+msg.text;
 			break;
 		case "sessions":
 			sessions = msg.sessions;
